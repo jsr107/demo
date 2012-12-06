ba942aa38db6f830f045545e643b78ef8092a94e
Brian Oliver <brian.oliver@oracle.com>
2012-12-06 15:34:28 -0500

    1. Issue #83: Introduced SimpleCacheConfiguration (and corrected CacheConfiguration method name for isTransactionEnabled) 2. Refactored the demo to use the new SimpleCacheConfiguration

--- src/test/java/javax/cache/CacheTest.java
+++ src/test/java/javax/cache/CacheTest.java
@@ -2,10 +2,10 @@ package javax.cache;
 
 import org.junit.Test;
 
-//import java.util.concurrent.TimeUnit;
-//
-//import static junit.framework.Assert.assertEquals;
-//import static junit.framework.Assert.assertNull;
+import java.util.concurrent.TimeUnit;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNull;
 
 /**
  * @author Greg Luck
@@ -14,26 +14,23 @@ public class CacheTest {
 
     @Test
     public void simpleAPI() {
-//TODO: we'll fix the following when Issue #83 is resolved
-//        String cacheName = "sampleCache";
-//        CacheManager cacheManager = Caching.getCacheManager();
-//
-//        SimpleCacheConfigurationBuilder<String, Integer> builder = new SimpleCacheConfigurationBuilder<String, Integer>();
-//        builder.setStoreByValue(false)
-//               .setStatisticsEnabled(true)
-//               .setCacheEntryExpiryPolicy(new CacheEntryExpiryPolicy.Accessed<String, Integer>(new CacheConfiguration.Duration(TimeUnit.HOURS, 1)));
-//        CacheConfiguration<String, Integer> cacheConfiguration = builder.build();
-//        
-//        Cache<String, Integer> cache = cacheManager.configureCache(cacheName, cacheConfiguration);
-//
-//        String key = "key";
-//        Integer value1 = 1;
-//        cache.put(key, value1);
-//        Integer value2 = cache.get(key);
-//        assertEquals(value1, value2);
-//
-//        cache.remove("key");
-//        assertNull(cache.get("key"));
-    }
+        String cacheName = "sampleCache";
+        CacheManager cacheManager = Caching.getCacheManager();
 
+        SimpleCacheConfiguration<String, Integer> config = new SimpleCacheConfiguration<String, Integer>();
+        config.setStoreByValue(false)
+              .setCacheEntryExpiryPolicy(new CacheEntryExpiryPolicy.Accessed<String, Integer>(new CacheConfiguration.Duration(TimeUnit.HOURS, 1)))
+              .setStatisticsEnabled(true);
+        
+        Cache<String, Integer> cache = cacheManager.configureCache(cacheName, config);
+
+        String key = "key";
+        Integer value1 = 1;
+        cache.put(key, value1);
+        Integer value2 = cache.get(key);
+        assertEquals(value1, value2);
+
+        cache.remove("key");
+        assertNull(cache.get("key"));
+    }
 }
