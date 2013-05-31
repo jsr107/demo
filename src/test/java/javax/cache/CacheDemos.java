15700fab24c00c91dbb87007b342c315d04a68c3
gregrluck <gluck@gregluck.com>
2013-05-31 11:14:52 -0700

    #134 Refactor configuration related classes and interfaces into new configuration package.

--- src/test/java/javax/cache/CacheDemos.java
+++ src/test/java/javax/cache/CacheDemos.java
@@ -2,11 +2,11 @@ package javax.cache;
 
 import org.junit.Test;
 
+import javax.cache.configuration.FactoryBuilder;
+import javax.cache.configuration.MutableConfiguration;
 import javax.cache.expiry.Accessed;
 import javax.cache.expiry.Duration;
-import javax.cache.expiry.ExpiryPolicy;
 import javax.cache.spi.CachingProvider;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static junit.framework.Assert.assertNull;
@@ -18,6 +18,7 @@ import static org.junit.Assert.assertEquals;
  */
 public class CacheDemos {
 
+
   @Test
   public void simpleAPI() {
     String cacheName = "sampleCache";
@@ -27,20 +28,45 @@ public class CacheDemos {
     MutableConfiguration<String, Integer> config = new MutableConfiguration<String, Integer>();
     config.setStoreByValue(false)
         .setTypes(String.class, Integer.class)
-        .setExpiryPolicyFactory(Factories.of(new Accessed<String, Integer>(new Duration(TimeUnit.HOURS, 1))))
+        .setExpiryPolicyFactory(FactoryBuilder.factoryOf(new Accessed<String, Integer>(new Duration(TimeUnit.HOURS, 1))))
         .setStatisticsEnabled(true);
 
     Cache<String, Integer> cache = cacheManager.configureCache(cacheName, config);
-
     String key = "key";
     Integer value1 = 1;
-    cache.put(key, value1);
+    cache.put("key", value1);
     Integer value2 = cache.get(key);
     assertEquals(value1, value2);
 
     cache.remove("key");
     assertNull(cache.get("key"));
   }
+
+  @Test
+  public void simpleAPINoGenerics() {
+    String cacheName = "sampleCache";
+    CachingProvider cachingProvider = Caching.getCachingProvider();
+    CacheManager cacheManager = cachingProvider.getCacheManager();
+
+    MutableConfiguration config = new MutableConfiguration<String, Integer>()
+        .setTypes(String.class, Integer.class);
+    config.setStoreByValue(false)
+        .setExpiryPolicyFactory(FactoryBuilder.factoryOf(new Accessed<String, Integer>(new Duration(TimeUnit.HOURS, 1))))
+        .setStatisticsEnabled(true);
+
+    Cache cache = cacheManager.configureCache(cacheName, config);
+    String key = "key";
+    Integer value1 = 1;
+    cache.put("key", value1);
+    //wrong
+    cache.put(value1, "key1");
+    Integer value2 = (Integer) cache.get(key);
+    assertEquals(value1, value2);
+
+    cache.remove("key");
+    assertNull(cache.get("key"));
+  }
+
 
 
 }
